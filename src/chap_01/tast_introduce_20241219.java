3171ec8851cd2dbe2c3047dc85e01f37838e6f12
jisu5 <jisu508@naver.com>
2024-12-19 21:30:23 +0900

    jisu

---
+++ src/chap_01/tast_introduce_20241219.java
@@ -0,0 +1,9 @@
+package chap_01;
+
+public class tast_introduce_20241219 {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요");
+        System.out.println("저는 22살 입니다");
+        System.out.println("Java를 공부하고 있습니다");
+    }
+}
